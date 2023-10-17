ae1c4abf8e87ffd46681edaa56e8bbfeea2551d1
iam-pritish <in.pritish@gmail.com>
2023-10-17 20:45:15 +0530

    done

--- gitdemo.py
+++ gitdemo.py
@@ -8,7 +8,10 @@ class Emp:
         print(self.name)
         print(self.age)
 emp1 = Emp ('Pritish', 27)
-emp1.dispay()
+emp1.display()
 
 emp2 = Emp ('Prajyot', 26)
 Emp.display(emp2)
+
+print("This Employee-01 has Name:- ", emp1.name)
+print("This Employee-02 age is:-", emp2.age)
