98562a872c643caba63916b0f80e8baccf3199aa
iam-pritish <in.pritish@gmail.com>
2023-10-17 20:39:11 +0530

    Commited

--- gitdemo.py
+++ gitdemo.py
@@ -4,4 +4,11 @@ class Emp:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-    def    
+    def display(self):
+        print(self.name)
+        print(self.age)
+emp1 = Emp ('Pritish', 27)
+emp1.dispay()
+
+emp2 = Emp ('Prajyot', 26)
+Emp.display(emp2)
