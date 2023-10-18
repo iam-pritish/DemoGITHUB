442e8502dc28a099038152a295864f50a4e52803
PRITISH <141925240+iam-pritish@users.noreply.github.com>
2023-10-18 13:18:41 +0530

    Create test2.py

---
+++ test2.py
@@ -0,0 +1,14 @@
+ =55
+print(a)
+
+class sum:
+  def __init__(self, add, sub):
+    self.add = add
+    self.sub = sub
+  def display(self):
+    print(self.add)
+    print(self.sub)
+num1 = sum(23 , 32)
+num1.display()
+sum.display(num1)
+print("Print the add number" , num1.add)
