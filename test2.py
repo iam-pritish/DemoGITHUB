1f1f0601c204f6e578991901031e20bd741fe934
iam-pritish <in.pritish@gmail.com>
2023-10-18 13:48:59 +0530

    more code added

--- test2.py
+++ test2.py
@@ -10,4 +10,21 @@ class sum:
     print(self.sub)
 num1 = sum(23 , 32)
 num1.display()
+sum.display(num1)
+print("Print the add number" , num1.add)
 
+# This is the example of OOPs concept where we can initite the class which helps to re-use the code in verious forms 
+# It is very good concept to create lrager code into shorter formate.
+
+class Emp:
+  def __init__(self, id, salary, exp):
+    self.id = id
+    self.salary = salary
+    self.exp = exp
+  def diaplay(self):
+    print(self.id)
+    print(self.salary)
+    print(self.exp)
+emp1 = Emp(88 , 120000, "4 years 6 months")
+emp1.diaplay()    
+     
